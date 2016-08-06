cdbb968236e2f8e8baf3e9f4d108e45448c456a1
Ravin Singh D <ravinsinghd@outlook.com>
2016-08-07 00:31:04 +0530

    created app component

---
+++ src/script/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '../../node_modules/@angular/core';
+
+@Component({
+    selector:'deviceready',
+    template:'<p class="event received">Device is Ready</p>'
+})
+export class AppComponent{
+
+}
